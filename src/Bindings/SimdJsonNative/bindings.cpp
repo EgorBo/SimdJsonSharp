1809d32ed20103c951c67824d1f44eab2195e1ae
EgorBo <egorbo@gmail.com>
2019-03-12 02:36:15 +0300

    Add an alternative option - C# bindings for native lib

---
+++ src/Bindings/SimdJsonNative/bindings.cpp
@@ -0,0 +1,64 @@
+#include "simdjson.h"
+
+
+#if (defined WIN32 || defined _WIN32)
+#define EXPORTS(returntype) extern "C" __declspec(dllexport) returntype __cdecl
+#else
+#define EXPORTS(returntype) extern "C" __attribute__((visibility("default"))) returntype
+#endif
+
+// ParsedJson
+EXPORTS(ParsedJson*) ParsedJson_ParsedJson()
+{
+	ParsedJson* pj = new ParsedJson();
+
+	return pj;
+}
+EXPORTS(bool) ParsedJson_allocateCapacity(ParsedJson* target, size_t len, size_t maxdepth = DEFAULTMAXDEPTH) { return target->allocateCapacity(len, maxdepth); }
+EXPORTS(bool) ParsedJson_isValid(ParsedJson* target) { return target->isValid(); }
+EXPORTS(void) ParsedJson_deallocate(ParsedJson* target) { target->deallocate(); }
+EXPORTS(void) ParsedJson_init(ParsedJson* target) { target->init(); }
+EXPORTS(void) ParsedJson_write_tape(ParsedJson* target, uint64_t val, uint8_t c) { target->write_tape(val, c); }
+EXPORTS(void) ParsedJson_write_tape_s64(ParsedJson* target, int64_t i) { target->write_tape_s64(i); }
+EXPORTS(void) ParsedJson_write_tape_double(ParsedJson* target, double d) { target->write_tape_double(d); }
+EXPORTS(uint32_t) ParsedJson_get_current_loc(ParsedJson* target) { return target->get_current_loc(); }
+EXPORTS(void) ParsedJson_annotate_previousloc(ParsedJson* target, uint32_t saved_loc, uint64_t val) { target->annotate_previousloc(saved_loc, val); }
+EXPORTS(void) ParsedJson_dispose(ParsedJson* target) { target->~ParsedJson(); }
+
+// ParsedJson::iterator
+EXPORTS(void*) iterator_iterator(void* pj)
+{
+	ParsedJson* cc = static_cast<ParsedJson*>(pj);
+	auto len = cc->bytecapacity;
+
+	return new ParsedJson::iterator(*cc);
+}
+EXPORTS(bool) iterator_isOk(ParsedJson::iterator* target) { return target->isOk(); }
+EXPORTS(size_t) iterator_get_tape_location(ParsedJson::iterator* target) { return target->get_tape_location(); }
+EXPORTS(size_t) iterator_get_tape_length(ParsedJson::iterator* target) { return target->get_tape_length(); }
+EXPORTS(size_t) iterator_get_depth(ParsedJson::iterator* target) { return target->get_depth(); }
+EXPORTS(uint8_t) iterator_get_scope_type(ParsedJson::iterator* target) { return target->get_scope_type(); }
+EXPORTS(bool) iterator_move_forward(ParsedJson::iterator* target) { return target->move_forward(); }
+EXPORTS(uint8_t) iterator_get_type(ParsedJson::iterator* target) { return target->get_type(); }
+EXPORTS(int64_t) iterator_get_integer(ParsedJson::iterator* target) { return target->get_integer(); }
+EXPORTS(const char*) iterator_get_string(ParsedJson::iterator* target) { return target->get_string(); }
+EXPORTS(double) iterator_get_double(ParsedJson::iterator* target) { return target->get_double(); }
+EXPORTS(bool) iterator_is_object_or_array(ParsedJson::iterator* target) { return target->is_object_or_array(); }
+EXPORTS(bool) iterator_is_object(ParsedJson::iterator* target) { return target->is_object(); }
+EXPORTS(bool) iterator_is_array(ParsedJson::iterator* target) { return target->is_array(); }
+EXPORTS(bool) iterator_is_string(ParsedJson::iterator* target) { return target->is_string(); }
+EXPORTS(bool) iterator_is_integer(ParsedJson::iterator* target) { return target->is_integer(); }
+EXPORTS(bool) iterator_is_double(ParsedJson::iterator* target) { return target->is_double(); }
+EXPORTS(bool) iterator_move_to_key(ParsedJson::iterator* target, char* key) { return target->move_to_key(key); }
+EXPORTS(bool) iterator_next(ParsedJson::iterator* target) { return target->next(); }
+EXPORTS(bool) iterator_prev(ParsedJson::iterator* target) { return target->prev(); }
+EXPORTS(bool) iterator_up(ParsedJson::iterator* target) { return target->up(); }
+EXPORTS(bool) iterator_down(ParsedJson::iterator* target) { return target->down(); }
+EXPORTS(void) iterator_to_start_scope(ParsedJson::iterator* target) { target->to_start_scope(); }
+EXPORTS(bool) iterator_is_object_or_array_static(uint8_t type) { return ParsedJson::iterator::is_object_or_array(type); }
+EXPORTS(void) iterator_dispose(ParsedJson::iterator* target) { target->~iterator(); }
+
+// Global
+EXPORTS(char*) Global_allocate_padded_buffer(size_t length) { return allocate_padded_buffer(length); }
+EXPORTS(size_t) Global_jsonminify(const uint8_t* buf, size_t len, uint8_t* out) { return jsonminify(buf, len, out); }
+EXPORTS(int) Global_json_parse(const uint8_t* buf, size_t len, ParsedJson* pj, bool reallocifneeded = true) { return json_parse(buf, len, *pj, reallocifneeded); }
---
+++ src/Bindings/SimdJsonSharp.Bindings/Bindings.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+#region stdint types and friends
+using size_t = System.IntPtr; // IntPtr here!!
+using uint8_t = System.Byte;
+using uint64_t = System.UInt64;
+using uint32_t = System.UInt32;
+using int64_t = System.Int64;
+using bytechar = System.SByte;
+#endregion
+
+namespace HelloWorld
+{
+    public static unsafe class UtilsN // 'N' stands for Native
+    {
+        public const string NativeLib = @"SimdJsonNative";
+
+        public static uint JsonMinify(uint8_t* buf, uint len, uint8_t* output) => (uint)Global_jsonminify(buf, (size_t)len, output);
+        public static int JsonParse(uint8_t* buf, uint len, ParsedJsonN pj, bool reallocifneeded = true) => Global_json_parse(buf, (size_t)len, pj.Handle, reallocifneeded);
+
+        public static ParsedJsonN BuildParsedJson(uint8_t* buf, uint len, bool reallocifneeded = true)
+        {
+            ParsedJsonN pj = new ParsedJsonN();
+            bool ok = pj.AllocateCapacity(len);
+            if (ok)
+            {
+                Global_json_parse(buf, (size_t)len, pj.Handle, reallocifneeded);
+            }
+            else
+            {
+                throw new InvalidOperationException("failure during memory allocation");
+            }
+            return pj;
+        }
+
+
+        #region pinvokes
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bytechar* Global_allocate_padded_buffer(size_t length);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern size_t Global_jsonminify(uint8_t* buf, size_t len, uint8_t* output);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern int Global_json_parse(uint8_t* buf, size_t len, void* pj, bool reallocifneeded = true);
+        #endregion
+    }
+
+    public unsafe class ParsedJsonN : IDisposable // 'N' stands for Native
+    {
+        public void* Handle { get; private set; }
+        public ParsedJsonN(void* handle) => this.Handle = handle;
+        public ParsedJsonN() => Handle = ParsedJson_ParsedJson();
+        public bool AllocateCapacity(uint len, uint maxdepth = 1024) => ParsedJson_allocateCapacity(Handle, len, maxdepth);
+        public bool IsValid() => ParsedJson_isValid(Handle);
+        public void Deallocate() => ParsedJson_deallocate(Handle);
+        public void Init() => ParsedJson_init(Handle);
+        public void WriteTape(uint64_t val, uint8_t c) => ParsedJson_write_tape(Handle, val, c);
+        public void WriteTapeS64(int64_t i) => ParsedJson_write_tape_s64(Handle, i);
+        public void WriteTapeDouble(double d) => ParsedJson_write_tape_double(Handle, d);
+        public uint32_t GetCurrentLoc() => ParsedJson_get_current_loc(Handle);
+        public void AnnotatePreviousLoc(uint32_t savedLoc, uint64_t val) => ParsedJson_annotate_previousloc(Handle, savedLoc, val);
+
+
+        #region pinvokes
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void* ParsedJson_ParsedJson();
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool ParsedJson_allocateCapacity(void* target, uint len, uint maxdepth);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool ParsedJson_isValid(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void ParsedJson_deallocate(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void ParsedJson_init(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void ParsedJson_write_tape(void* target, uint64_t val, uint8_t c);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void ParsedJson_write_tape_s64(void* target, int64_t i);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void ParsedJson_write_tape_double(void* target, double d);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern uint32_t ParsedJson_get_current_loc(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void ParsedJson_annotate_previousloc(void* target, uint32_t saved_loc, uint64_t val);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void ParsedJson_dispose(void* target);
+        #endregion
+
+        public void Dispose()
+        {
+            if (Handle != (void*)IntPtr.Zero)
+            {
+                ParsedJson_dispose(Handle);
+                Handle = (void*)IntPtr.Zero;
+            }
+        }
+
+        ~ParsedJsonN()
+        {
+            if (Handle != (void*)IntPtr.Zero)
+                Dispose();
+        }
+    }
+
+    public unsafe class ParsedJsonIteratorN : IDisposable
+    {
+        public void* Handle { get; private set; }
+        public ParsedJsonIteratorN(void* handle) => this.Handle = handle;
+        public ParsedJsonIteratorN(ParsedJsonN pj) => Handle = iterator_iterator(pj.Handle);
+        public bool IsOk() => iterator_isOk(Handle);
+        public uint GetTapeLocation() => (uint)iterator_get_tape_location(Handle);
+        public uint GetTapeLength() => (uint)iterator_get_tape_length(Handle);
+        public uint GetDepth() => (uint)iterator_get_depth(Handle);
+        public uint8_t GetScopeType() => iterator_get_scope_type(Handle);
+        public bool MoveForward() => iterator_move_forward(Handle);
+        public uint8_t GetCurrentType() => iterator_get_type(Handle);
+        public int64_t GetInteger() => iterator_get_integer(Handle);
+        public bytechar* GetUtf8String() => iterator_get_string(Handle);
+        public double GetDouble() => iterator_get_double(Handle);
+        public bool IsObjectOrArray() => iterator_is_object_or_array(Handle);
+        public bool IsObject() => iterator_is_object(Handle);
+        public bool IsArray() => iterator_is_array(Handle);
+        public bool IsString() => iterator_is_string(Handle);
+        public bool IsInteger() => iterator_is_integer(Handle);
+        public bool IsDouble() => iterator_is_double(Handle);
+        public bool MoveToKey(bytechar* key) => iterator_move_to_key(Handle, key);
+        public bool Next() => iterator_next(Handle);
+        public bool Prev() => iterator_prev(Handle);
+        public bool Up() => iterator_up(Handle);
+        public bool Down() => iterator_down(Handle);
+        public void ToStartScope() => iterator_to_start_scope(Handle);
+
+        #region pinvokes
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void* iterator_iterator(void* pj);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_isOk(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern size_t iterator_get_tape_location(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern size_t iterator_get_tape_length(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern size_t iterator_get_depth(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern uint8_t iterator_get_scope_type(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_move_forward(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern uint8_t iterator_get_type(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern int64_t iterator_get_integer(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bytechar* iterator_get_string(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern double iterator_get_double(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_is_object_or_array(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_is_object(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_is_array(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_is_string(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_is_integer(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_is_double(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_move_to_key(void* target, bytechar* key);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_next(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_prev(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_up(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern bool iterator_down(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void iterator_to_start_scope(void* target);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void iterator_is_object_or_array_static(uint8_t type);
+        [DllImport(UtilsN.NativeLib, CallingConvention = CallingConvention.Cdecl)] private static extern void iterator_dispose(void* target);
+        #endregion
+
+        public void Dispose()
+        {
+            if (Handle != (void*) IntPtr.Zero)
+            {
+                iterator_dispose(Handle);
+                Handle = (void*) IntPtr.Zero;
+            }
+        }
+
+        ~ParsedJsonIteratorN()
+        {
+            if (Handle != (void*)IntPtr.Zero) Dispose();
+        }
+    }
+}
